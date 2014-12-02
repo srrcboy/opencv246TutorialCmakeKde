d1b1ccb6367bb02591559e928034b9abeb491688
srrcboy <b-he@163.com>
2014-12-02 22:06:23 +0800

    create project and test opencv on kde

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
+#include <opencv2/opencv.hpp>
 #include <iostream>
 
-int main(int argc, char **argv) {
-    std::cout << "Hello, world!" << std::endl;
+using namespace cv;
+using namespace std;
+
+
+int main(int argc, char **argv)
+{
+    Mat image=imread("../../images/lena.jpg");
+    namedWindow("test",CV_WINDOW_AUTOSIZE);
+    imshow("test",image);
+    waitKey(0);
     return 0;
 }
